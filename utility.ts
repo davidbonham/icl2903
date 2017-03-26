9d2d173be0cf26361cb079e287674a61ccb7def0
David Bonham <david@davidbonham.org.uk>
2017-03-26 11:29:32 +0100

    new files for generator model

---
+++ utility.ts
@@ -0,0 +1,25 @@
+namespace Utility {
+
+    export function basicDate(date : Date) : string  {
+
+        // For items that can be one or two characters, prefix with a 0
+        // and take the last two characters. Note that month is 0-based
+        const dd = ("0" + date.getDate()).slice(-2);
+        const mm = ("0" + (date.getMonth()+1)).slice(-2);
+        const yy = date.getFullYear().toString().slice(-2);
+
+        return dd+"/"+mm+"/"+yy
+    }
+
+    export function basicTime(date : Date) : string  {
+
+        // For items that can be one or two characters, prefix with a 0
+        // and take the last two characters
+        const hh = ("0" + date.getHours()).slice(-2);
+        const mm = ("0" + date.getMinutes()).slice(-2);
+        const ss = ("0" + date.getSeconds()).slice(-2);
+
+        return hh+":"+mm+":"+ss
+    }
+
+}
