f23971f5ef8d0dfcef606cb43b97b2bc54243e70
David Bonham <david@davidbonham.org.uk>
2017-04-20 13:05:38 +0100

    Add some comments - no functional change

--- commands/account.ts
+++ commands/account.ts
@@ -1,4 +1,8 @@
-
+/**
+ * The ACCOUNT command tells the user how much of their allocated quota
+ * they have used. A bucket of disc space is 128 words and the ICL 2903
+ * is a 24-bit machine so this corresponds to 128*3 = 384 bytes per bucket.
+ */
 class AccountCmd extends Command
 {
     public execute(session: Session.Session) : void {
@@ -6,10 +10,12 @@ class AccountCmd extends Command
         // Get the last saved settings from disc
         const account = session.fileStore.getAccount();
 
-        // Update them with the values used this session. We won't save the results
+        // Update them with the values used this session. We won't save
+        // the results
         account.update(1, session.elapsed(), session.mill(), true);
 
-        // Get the results and display them
+        // Format the values into strings padded with leading zeros. Note
+        // that the disc space is presented in a 4 character field.
         const logins    = Utility.padInteger(account.logins, 5, "0")
         const maxLogins = Utility.padInteger(account.maxLogins, 5, "0")
         const time      = Utility.padInteger(account.time, 5, "0")
--- commands/disc.ts
+++ commands/disc.ts
@@ -1,3 +1,9 @@
+/**
+ * The DISC command tells the user how much of their allocated disc quota
+ * they have used. A bucket of disc space is 128 words and the ICL 2903
+ * is a 24-bit machine so this corresponds to 128*3 = 384 bytes per bucket.
+ */
+
 class DiscCmd extends Command {
 
     public execute(session: Session.Session) : void {
@@ -8,9 +14,10 @@ class DiscCmd extends Command {
         // Update them with the values used this session. We won't save the results
         account.update(1, session.elapsed(), session.mill(), true);
 
+        // Format the values into strings padded with leading zeros.
         const disc    = Utility.padInteger(account.disc, 4, "0")
         const maxDisc = Utility.padInteger(account.maxDisc, 4, "0")
+
         session.println("DISC USED = " + disc + " BUCKETS  MAX = " + maxDisc + " BUCKETS")
-        console.log(new Error().stack)
     }
 }
